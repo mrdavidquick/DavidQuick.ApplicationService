00dcba9a8b4943f8067590a500e08cec43508949
David <david@davidquick.co.uk>
2024-11-15 15:47:42 +0000

    Helper for Application Creation

---
+++ Services.Applications.Tests/ApplicationTestHelper.cs
@@ -0,0 +1,28 @@
+﻿using Services.Common.Abstractions.Model;
+
+namespace Services.Applications.Tests
+{
+    internal static class ApplicationTestHelper
+    {
+        private const decimal InitialMinimumPayment = 0.99m;
+        private const decimal InitialMaximumPayment = 4000.00m;
+
+        internal static decimal GenerateRandomPayment()
+        {
+            var random = new Random();
+            const double range = (double)(InitialMaximumPayment - InitialMinimumPayment);
+            var sample = random.NextDouble() * range + (double)InitialMinimumPayment;
+            return (decimal)sample;
+        }
+
+        internal static Application GetEmptyProductOneApplication()
+        {
+            return new Application
+            {
+                Id = Guid.NewGuid(),
+                ProductCode = ProductCode.ProductOne,
+            };
+        }
+    }
+}
+
